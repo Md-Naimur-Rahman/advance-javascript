1ed6bc8c347544c1150c4805d3d7fe7b54926b5c
Md Naimur Rahman <naimur907@gmail.com>
2020-03-09 02:33:22 +0600

    array split slice splice join

---
+++ array-split.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+const num = [1,2,3,4,5,6,7,8];
+// const part = num.slice(2,5);
+//console.log(part);
+
+
+const removed = num.splice(2,5,77,88,99);
+// console.log(removed );
+// console.log(num);
+
+const together= num.join(",");
+
+
+console.log(together);
