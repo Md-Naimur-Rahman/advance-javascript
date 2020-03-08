9e179409a1e642fff3849d329cce838a75dac8d8
Md Naimur Rahman <naimur907@gmail.com>
2020-03-08 21:48:09 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,42 @@
+
+/*
+ let pakhi;  we will get undefined here
+
+function likhlam but return korlam na or ki return korlam ta declare kori nai, undefined dekhabe
+                                example :
+                                function add(num1,num2){
+                                    console.log(num1+num2);
+                                }
+                                const result = add(10,20);
+                                console.log(result);
+
+
+
+function er parameter number 2 ta, but ami pathalam akta undefined dekhabe
+
+                                                    example:
+                                                    function add(num1,num2){
+                                                        console.log(num1+num2);
+
+                                                    }
+
+                                                    const result = add(10);
+
+                                                    console.log(result);
+
+
+ output a amon akta property show korte chailam , but oita object a nai.. undefined dekhabe 
+                                    example: const student = {name:"Naimur", phone:5286};
+                                    console.log(student.fathername);
+
+
+let fun = undefined
+console.log(fun);
+
+
+
+ */
+
+
+let ages = [2,3,4];
+console.log(ages[10]);
